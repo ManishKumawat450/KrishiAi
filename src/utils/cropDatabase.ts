4b2e3e48980a9bf338b10be9b2d825b2436c583d
Manish Kumawat <161554095+ManishKumawat450@users.noreply.github.com>
2026-03-28 13:12:59 +0530

    Create crop recommendations database for Indian crops

---
+++ src/utils/cropDatabase.ts
@@ -0,0 +1,29 @@
+const cropRecommendations = {
+    "Wheat": {
+        "soil_pH": [6.0, 7.5],
+        "temperature": [10, 25],
+        "rainfall": [50, 100]
+    },
+    "Rice": {
+        "soil_pH": [5.5, 7.0],
+        "temperature": [20, 35],
+        "rainfall": [100, 200]
+    },
+    "Cotton": {
+        "soil_pH": [5.0, 8.5],
+        "temperature": [15, 35],
+        "rainfall": [50, 100]
+    },
+    "Sugarcane": {
+        "soil_pH": [6.0, 8.0],
+        "temperature": [20, 32],
+        "rainfall": [100, 150]
+    },
+    "Maize": {
+        "soil_pH": [5.5, 7.5],
+        "temperature": [10, 30],
+        "rainfall": [60, 120]
+    }
+};
+
+export default cropRecommendations;
